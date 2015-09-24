69311dd34dd797b144b5eb96897fbc80f7522848
Pablo Ramon <pabloramon044@gmail.com>
2015-09-24 18:55:14 -0300

    drive_gameOfLife.cpp agora recebe arquivo de texto como entrada e adquire o numero de linhas e colunas que o tabuleiro vai ter

--- src/drive_gameOfLife.cpp
+++ src/drive_gameOfLife.cpp
@@ -8,7 +8,16 @@ int main(int argc, char *argv[]){
 	/*Recebendo arquivo com configuração inicial da linha de comando*/
 	fstream confInit;
 	confInit.open(argv[1]);
-	
+	/*Pegar tamanho da matriz e o caracter que vai ser usado como célula viva*/
+	int size_x,size_y;
+	char alive_Cell;
+	confInit >> size_x;
+	confInit >> size_y;
+	confInit >> alive_Cell;
+
+	cout << "Size X: [" << size_x << "] ;" << endl;
+	cout << "Size Y: [" << size_y << "] ;" << endl;
+	cout << "Alive Cell: [" << alive_Cell << "] ;" << endl;
 
 	
 	
