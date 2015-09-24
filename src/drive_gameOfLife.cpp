8bc1a9fe494e60c0cb6088a03971b8c9f8859f26
Pablo Ramon <pabloramon044@gmail.com>
2015-09-24 19:33:12 -0300

    drive pega matriz em arquivo de texto e transforma em matriz de caracteres

--- src/drive_gameOfLife.cpp
+++ src/drive_gameOfLife.cpp
@@ -14,13 +14,35 @@ int main(int argc, char *argv[]){
 	confInit >> size_x;
 	confInit >> size_y;
 	confInit >> alive_Cell;
-
+	/*
 	cout << "Size X: [" << size_x << "] ;" << endl;
 	cout << "Size Y: [" << size_y << "] ;" << endl;
 	cout << "Alive Cell: [" << alive_Cell << "] ;" << endl;
+	*/
+	char **init;
+	init = new char*[size_x];
+	for(auto i(0);i<size_x;i++){
+		init[i] = new char[size_y];
+	}
 
-	
-	
+	for(auto i(0);i<size_x;i++){
+		for(auto j(0);j<size_y;j++){
+			confInit.get(init[i][j]);
+		}
+	}
+	for(auto i(0);i<size_x;i++){
+		for(auto j(0);j<size_y;j++){
+			cout << init[i][j];
+		}
+	}
+	/*string init[size_x];
+
+	for(auto i(0);i<size_x;i++){
+		getline(confInit,init[i]);
+	}
+	for(auto i(0);i<size_x;i++){
+		cout << init[i] << endl;
+	}*/
 
 	Life l(7,28);//((int)nTam[0],(int)nTam[2],(char)aliveCell);
 
