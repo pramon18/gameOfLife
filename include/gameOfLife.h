e44c6745a4b938c71da9efafba5ba787103fbb03
Pablo Ramon <pabloramon044@gmail.com>
2015-09-24 23:10:02 -0300

    Remodelagem de alguns métodos e parâmetros

--- include/gameOfLife.h
+++ include/gameOfLife.h
@@ -1,13 +1,13 @@
 #ifndef _GAME_OF_LIFE_H
 #define _GAME_OF_LIFE_H
-
+#include <iostream>
 /*Tamanho Default do tabuleiro de simulação*/
 const int nLinDefault = 15, nColDefault = 15;
 const char _alive_cell = 'k';
 
 class Life{
 	/*Parâmetros e Métodos privados*/
-	char **tab,**tabStable, alive_cell;
+	char **tab,**tabStable, **tabUpdate, alive_cell;
 	int nLin, nCol, generation=0;
 	
 
@@ -23,12 +23,14 @@ class Life{
 
 		/*Métodos públicos da classe Life*/
 		void fillExist(char **_tab1);
-		void update(const Life &_tab1);
-		void setAlive(const Life _tab1);
-		void print(const Life _tab1);
+		void update();
+		void setAlive();
+		void print();
 		Life operator =(const Life &_tab1);
 		bool operator ==(const Life &_tab1);
-		bool stable(const Life _tab1,const Life _tab2);
+		bool stable();
+		int check(char s);
+		
 };
 
 #endif
